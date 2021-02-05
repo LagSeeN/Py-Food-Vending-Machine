421be221cb78a7a14e0037815925ef0fc5c6dddc
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-02-05 23:45:57 +0700

    :heavy_plus_sign: update code

--- Main.py
+++ Main.py
@@ -1,6 +1,8 @@
 import sys
+import mongoDBServer
+import base64
 
-from PyQt5 import QtCore
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
 
 from MainScreen import Ui_MainForm
@@ -12,6 +14,39 @@ class MainWindows(QMainWindow):
         self.ui = Ui_MainForm()
         self.ui.setupUi(self)
         # Event
+        self.loadFoodItem()
+
+    def loadFoodItem(self):
+        row1 = 0
+        row2 = 0
+        count = mongoDBServer.countData()
+        imageItem = mongoDBServer.getAllImage()
+        _id = mongoDBServer.getAll_Id()
+        for i in range(count):
+            self.foodItem = QtWidgets.QPushButton(self.ui.scrollAreaWidgetContents)
+            self.foodItem.setMaximumSize(QtCore.QSize(280, 280))
+            self.foodItem.setText("")
+            base64_data = imageItem[i]
+            pm = QtGui.QPixmap()
+            pm.loadFromData(base64.b64decode(base64_data))
+            icon = QtGui.QIcon()
+            icon.addPixmap(pm, QtGui.QIcon.Normal, QtGui.QIcon.Off)
+            self.foodItem.setIcon(icon)
+            self.foodItem.setIconSize(QtCore.QSize(280, 280))
+            self.foodItem.clicked.connect(lambda ch, idItem=_id[i]: self.foodSelect(idItem))
+            self.ui.gridLayout.addWidget(self.foodItem, row1, row2, 1, 1)
+            # print("[{} , {} , 1 , 1]".format(row1, row2))
+            if row2 == 2:
+                row2 = 0
+                row1 += 1
+            else:
+                row2 += 1
+
+    def foodSelect(self, idItem):
+        print('{}'.format(idItem))
+        self.ui.label.setText('{}'.format(mongoDBServer.getFood(idItem)[0]['product_name']))
+        self.ui.label_7.setText('{}'.format(mongoDBServer.getFood(idItem)[0]['stock']))
+        self.ui.label_5.setText('{}'.format(mongoDBServer.getFood(idItem)[0]['price']))
 
 
 if __name__ == '__main__':
--- mongoDBServer.py
+++ mongoDBServer.py
@@ -1,9 +1,48 @@
 import pymongo
 
+server = 'mongodb+srv://vending_machine:wYvBQGPZHF6tMGbqHueyZZeWMjcuq47zSA6p9DifL3WeExvSi5RWE4hYuuwAcgG4ZoSNoXyYb37txCESZw6UfffmFrnuRZXP4Rpqd9LfRMEq8K3toKAwsuUcbakHvz58@clustertni.kt6oq.mongodb.net/Food_Vending_Machine?retryWrites=true&w=majority'
+
 
 def serchData():
-    server = 'mongodb+srv://vending_machine:wYvBQGPZHF6tMGbqHueyZZeWMjcuq47zSA6p9DifL3WeExvSi5RWE4hYuuwAcgG4ZoSNoXyYb37txCESZw6UfffmFrnuRZXP4Rpqd9LfRMEq8K3toKAwsuUcbakHvz58@clustertni.kt6oq.mongodb.net/Food_Vending_Machine?retryWrites=true&w=majority'
     with pymongo.MongoClient(server) as conn:
         db = conn.get_database('Food_Vending_Machine')
         found = db['products'].count_documents({})
         print('Data in {}'.format(found))
+
+
+def countData():
+    with pymongo.MongoClient(server) as conn:
+        db = conn.get_database('Food_Vending_Machine')
+        found = db['products'].count_documents({})
+        return found
+
+
+def getAllImage():
+    with pymongo.MongoClient(server) as conn:
+        db = conn.get_database('Food_Vending_Machine')
+        cursor = db['products'].find()
+        image = []
+        for i in cursor:
+            image.append(i['image'])
+        return image
+
+
+def getAll_Id():
+    with pymongo.MongoClient(server) as conn:
+        db = conn.get_database('Food_Vending_Machine')
+        cursor = db['products'].find()
+        _id = []
+        for i in cursor:
+            _id.append(i['_id'])
+        return _id
+
+
+def getFood(_id):
+    with pymongo.MongoClient(server) as conn:
+        db = conn.get_database('Food_Vending_Machine')
+        where = {'_id': _id}
+        cursor = db['products'].find(where)
+        food = []
+        for i in cursor:
+            food.append(i)
+        return food
