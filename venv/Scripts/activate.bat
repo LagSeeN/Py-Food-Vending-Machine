f6a2a5ffe3ad188948e55ca5c45c95974c4fdba0
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-02-05 23:41:03 +0700

    edit lib

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\LagSe\PycharmProjects\[ITE-428]Food_Vending_Machine\venv
+set VIRTUAL_ENV=C:\Users\LagSeeN\PycharmProjects\Py-Food-Vending-Machine\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
