e908ef32ee9811b0c0cfb77dc2805b7100da854b
LagSeeN <19259262+LagSeeN@users.noreply.github.com>
2021-02-07 22:02:45 +0700

    :bento: add fun wait coin input

--- InputCoin.py
+++ InputCoin.py
@@ -1,53 +1,93 @@
 import sys
 import mongoDBServer
+
 from bson import ObjectId
+from time import sleep
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow, QDialog
 from PyQt5.QtCore import QObject, QThread, pyqtSignal
 
 import InputCoinUI
+import CoinEmulator
 
 
-class getFood(QThread):
-    get_food = pyqtSignal(list, str)
+class WaitCoinInput(QThread):
+    update_Price = pyqtSignal(int)
+
+    def __init__(self, parent=None):
+        super(WaitCoinInput, self).__init__(parent)
+        self.time_left = 60
+
+    def run(self):
+        while True:
+            sleep(1)
+            self.update_Price.emit(self.time_left)
+            self.time_left -= 1
+
+    def stop(self):
+        self.terminate()
+
+
+class GetFood(QThread):
+    get_food = pyqtSignal(dict, str)
 
     def __init__(self, item, parent=None):
-        super(getFood, self).__init__(parent)
+        super(GetFood, self).__init__(parent)
         self.item = item
 
     def run(self):
-        item = mongoDBServer.getFood(self.item)
+        item = mongoDBServer.get_food(self.item)
         self.get_food.emit(item, str(self.item))
 
     def stop(self):
         self.terminate()
 
 
-class InputCoin(QDialog, InputCoinUI.Ui_Dialog):
-    def __init__(self, _id, parent=None):
+class InputCoin(QDialog, InputCoinUI.Ui_InputDialog):
+    def __init__(self, _id, MainScreen, parent=None):
         super(InputCoin, self).__init__(parent)
+        self.WaitCoinInput = WaitCoinInput()
+        self.CoinEmulator = CoinEmulator.CoinEmulator(self)
         self.setupUi(self)
+        # var
+        self.MainScreen = MainScreen
+        self.input_money = 0
+        self.total_price = 0
+        self.WaitCoinInputIsActive = False
         # แปลง str เป็น ObjectId
         self._id = ObjectId(_id)
-        self.getFoodThread = getFood(self._id)
+        self.getFoodThread = GetFood(self._id)
         self.getFoodThread.start()
-        self.getFoodThread.get_food.connect(self.foodShowInfomation)
+        self.getFoodThread.get_food.connect(self.food_show_information)
 
-    def foodShowInfomation(self, food, idItem):
+    def food_show_information(self, food, item_id):
         self.getFoodThread.stop()
-        # self.foodArea.setEnabled(True)
-        print('{}'.format(food[0]['product_name']))
-        # if food[0]['stock'] == 0:
-        #     self.btnPay.setEnabled(False)
-        #     self.lblStatus.setText('หมด')
-        # else:
-        #     self.btnPay.setEnabled(True)
-        #     self.lblStatus.setText('พร้อมจำหน่าย')
-        # self.lblStock.setText('{}'.format(food[0]['stock']))
-        # self.lblPrice.setText('{}'.format(food[0]['price']))
-        # self.groupBoxDetailFood.setVisible(True)
-        # self.currentSelect = idItem
+        self.total_price = food['price']
+        self.lbl_total_money.setText('{0:,g}'.format(self.total_price))
+        self.lbl_input_money.setText('{0:,g}'.format(self.input_money))
+        self.CoinEmulator.open()
+        self.WaitCoinInput.start()
+        self.WaitCoinInputIsActive = True
+        self.WaitCoinInput.update_Price.connect(self.wait_coin_input)
+
+    def wait_coin_input(self, time_left):
+        self.lbl_input_money.setText('{0:,g}'.format(self.input_money))
+        if time_left == 0:
+            self.WaitCoinInputIsActive = False
+            self.WaitCoinInput.stop()
+            self.CoinEmulator.close()
+            self.close()
+        if self.input_money >= self.total_price:
+            self.WaitCoinInputIsActive = False
+            self.WaitCoinInput.stop()
+            self.CoinEmulator.close()
+            print(self.input_money)
+
+    def closeEvent(self, event):
+        if self.WaitCoinInputIsActive:
+            self.WaitCoinInput.stop()
+        event.accept()
 
 
 if __name__ == '__main__':
